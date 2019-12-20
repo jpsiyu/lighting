9477ed2f2775d895bebd979d9f243e742333c8a1
wusiyu <stephenwu2019@163.com>
2019-12-20 11:36:57 +0800

    show demo

--- ui/ui.go
+++ ui/ui.go
@@ -5,11 +5,6 @@ import (
 )
 
 func Run() {
-	ins := demo.NewImage()
-	/*
-		images := []string{
-			"https://cn.bing.com/th?id=OIP.9j-saEMnwDuVhXq00rIH2AHaFj&pid=Api&rs=1",
-		}
-	*/
-	ins.Run(nil)
+	ins := demo.NewDemo()
+	ins.Run()
 }
