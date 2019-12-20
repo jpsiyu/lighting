82c806e4207bec5f0d6d5c46125c2880084b3fa8
wusiyu <stephenwu2019@163.com>
2019-12-20 14:01:36 +0800

    demo command

--- cmd/ui.go
+++ cmd/ui.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"github.com/jpsiyu/lighting/ui"
+	"github.com/jpsiyu/lighting/ui/demo"
 	"github.com/spf13/cobra"
 )
 
@@ -13,6 +14,17 @@ var uiCmd = &cobra.Command{
 	},
 }
 
+var uiDemoCmd = &cobra.Command{
+	Use:   "demo",
+	Short: "ui widgets demonstrate",
+	Run: func(cmd *cobra.Command, args []string) {
+		d := demo.NewDemo()
+		d.Run()
+	},
+}
+
 func init() {
 	rootCmd.AddCommand(uiCmd)
+
+	uiCmd.AddCommand(uiDemoCmd)
 }
